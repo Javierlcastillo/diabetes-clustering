0a5860d3407d7523be9cb6e38ea02391e090c82b
Javier <jazer2228@gmail.com>
2025-06-07 18:58:30 -0600

    modelo creado con API funcional

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import pickle
+from kmodes.kprototypes import KPrototypes
+
+# Cargar el modelo
+with open("kproto_model.pkl", "rb") as f:
+    kproto = pickle.load(f)
+
+app = Flask(__name__)
+
+# Ruta para predecir el cluster
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+
+    try:
+        # Entrada esperada: ['HighBP', 'HighChol', 'GenHlth', 'Sex', 'Diabetes', 'Age', 'BMI']
+        input_data = np.array([[
+            int(data['HighBP']),
+            int(data['HighChol']),
+            int(data['GenHlth']),
+            int(data['Sex']),
+            int(data['Diabetes']),
+            int(data['Age']),
+            float(data['BMI'])
+        ]], dtype=object)
+
+        # Índices categóricos
+        categorical_cols = [0, 1, 2, 3, 4, 5]
+
+        cluster = kproto.predict(input_data, categorical=categorical_cols)[0]
+        return jsonify({'cluster': int(cluster)})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
